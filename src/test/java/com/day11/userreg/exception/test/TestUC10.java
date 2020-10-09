5583ed13251224646e4200bd625129d011c736bc
vahbib <singhbibhav084@gmail.com>
2020-10-09 09:40:29 +0530

    Added UserCase 1 to 9 - user registration

--- src/test/java/com/day11/userreg/exception/test/TestUC10.java
+++ src/test/java/com/day11/userreg/exception/test/TestUC10.java
@@ -1,4 +1,9 @@
 package com.day11.userreg.exception.test;
 
+import com.day11.userreg.exception.UserRegistrationUC1to9;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestUC10 {
+
 }
