0c6966ac67ed7b31450b9d56edfa95621f8954bc
vahbib <singhbibhav084@gmail.com>
2020-10-13 18:09:26 +0530

    UserCase 13 - Refactor the Code to use Lambda Function to validate User Entry

--- src/main/java/com/day11/userreg/exception/UserCase13_LambdaExpression.java
+++ src/main/java/com/day11/userreg/exception/UserCase13_LambdaExpression.java
@@ -1,39 +1,70 @@
 package com.day11.userreg.exception;
 
-import javax.print.DocFlavor;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserCase13_LambdaExpression {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        static Scanner sc = new Scanner(System.in);
 
-        CheckPattern firstName = pattern -> pattern.matches("^[A-Z]{1}[a-z]{2,}$") ? true : false;
-        CheckPattern lastName = pattern -> pattern.matches("^[A-Z]{1}[a-z]{2,}$") ? true : false;
-        CheckPattern email = pattern -> pattern.matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$") ? true : false;
-        CheckPattern mobile = pattern -> pattern.matches("^[1-9]{2}\\s{1}[1-9]{1}[0-9]{9}$") ? true : false;
-        CheckPattern password = pattern -> pattern.matches("^(?=.*[0-9])"
-                + "(?=.*[a-z])(?=.*[A-Z])"
-                + "(?=.*[@#$%^&+=])"
-                + "(?=\\S+$).{8,20}$") ? true : false;
+        public static boolean CheckFName(String fName) throws FNameException {
+            CheckPattern firstName = (fNPatt) -> Pattern.matches("^[A-Z]{1}[a-z]{2,}$", fName);
+            if (firstName.validatePattern(fName)) {
+                System.out.println("Valid");
+                return true;
+            } else {
+                throw new FNameException("Invalid - First Name Exception");
+            }
+        }
+        public static boolean CheckLName(String lName) throws LNameException {
+            CheckPattern lastName = (lNPatt) -> Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lName);
+            if (lastName.validatePattern(lName)) {
+                System.out.println("Valid");
+                return true;
+            } else {
+                throw new LNameException("Invalid - Last Name Exception");
+            }
+        }
+        public static boolean CheckEmail(String email) throws EMailException {
+            CheckPattern eMail = (emailPatt) -> Pattern.matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$", email);
+            if (eMail.validatePattern(email)) {
+                System.out.println("Valid");
+                return true;
+            } else {
+                throw new EMailException("Invalid - Email Exception");
+            }
+        }
 
-        System.out.println("Enter Your Details to Validate, If Valid then 'true' else 'false'");
-        System.out.println("Enter First Name: ");
-        System.out.println(firstName.validatePattern(sc.nextLine()));
-        System.out.println("Enter Last Name: ");
-        System.out.println(lastName.validatePattern(sc.nextLine()));
-        System.out.println("Enter Email: ");
-        System.out.println(email.validatePattern(sc.nextLine()));
-        System.out.println("Enter Mobile Number: ");
-        System.out.println(mobile.validatePattern(sc.nextLine()));
-        System.out.println("Enter Password: ");
-        System.out.println(password.validatePattern(sc.nextLine()));
+        public static boolean CheckMobile(String mob) throws MobileException {
+            CheckPattern mobile = (mobPatt) -> Pattern.matches("^[1-9]{2}\\s{1}[1-9]{1}[0-9]{9}$", mob);
+            if (mobile.validatePattern(mob)) {
+                System.out.println("Valid");
+                return true;
+            } else {
+                throw new MobileException("Invalid - Mobile Exception");
+            }
+        }
+        public static boolean CheckPass(String pass) throws PassException {
+            CheckPattern password = (passPatt) -> Pattern.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s)[a-zA-Z0-9]*[^a-zA-Z0-9\\s]{1}[a-zA-Z0-9]*$", pass);
+            if (password.validatePattern(pass)) {
+                System.out.println("Valid");
+                return true;
+            } else {
+                throw new PassException("Invalid - Password Exception");
+            }
+        }
 
-    }
+        public static void main(String[] args) throws FNameException, LNameException, EMailException, MobileException, PassException {
+            UserCase13_LambdaExpression lambda = new UserCase13_LambdaExpression();
 
+            System.out.print("Enter First Name: "); lambda.CheckFName(sc.nextLine());
+            System.out.print("\nEnter Last Name: ");  lambda.CheckLName(sc.nextLine());
+            System.out.print("\nEnter Email: "); lambda.CheckEmail(sc.nextLine());
+            System.out.print("\nEnter Mobile Number: "); lambda.CheckMobile(sc.nextLine());
+            System.out.print("\nEnter Password: "); lambda.CheckPass(sc.nextLine());
+        }
 }
 @FunctionalInterface
 interface CheckPattern {
-    public boolean validatePattern(String pattern);
+    boolean validatePattern(String pattern);
 }
--- src/main/java/com/day11/userreg/exception/UserRegistrationException.java
+++ src/main/java/com/day11/userreg/exception/UserRegistrationException.java
@@ -1,5 +1,28 @@
-//package com.day11.userreg.exception;
-//
-//public class UserRegistrationException extends Exception {
-//    //UC 12 starts here
-//}
+package com.day11.userreg.exception;
+
+    //UC 12 starts here
+    class FNameException extends Exception {
+        FNameException(String f){
+            super(f);
+        }
+    }
+    class LNameException extends Exception {
+        LNameException(String l){
+            super(l);
+        }
+    }
+    class MobileException extends Exception {
+        MobileException(String m){
+            super(m);
+        }
+    }
+    class EMailException extends Exception {
+        EMailException(String e){
+            super(e);
+        }
+    }
+    class PassException extends Exception {
+        PassException(String p){
+            super(p);
+        }
+    }
